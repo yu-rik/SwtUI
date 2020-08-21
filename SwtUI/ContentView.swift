eabe9078816005d63e3de5d440de1c6aba89e008
Yurii Nosachenko <yurik_n@outlook.com>
2020-08-21 21:18:55 +0300

    Initial Commit

---
+++ SwtUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwtUI
+//
+//  Created by yurik on 8/21/20.
+//  Copyright © 2020 yurik. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
